3b336f1ac6e94e216dcf8c5c19e68ab21abac61b
MaxKatafalk <max20060602@mail.ru>
2025-10-04 23:36:46 +1000

    форма обратной связи

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+	name = ""
+	email = ""
+	message = ""
+
+	if request.method == 'POST':
+		name = request.form['name'].strip()
+		email = request.form['email'].strip()
+		message = request.form['message'].strip()
+		return f"<h1> спасибо за ваше сообщение!</h1>"\
+				f"<p> Имя: {name}</p>"\
+				f"<p>  Email: {email}</p>"\
+				f"<p>  Сообщение: {message}</p>"
+
+	
+	return render_template('feedback.html', name=name, email=email, message=message)
+	
+
+
+		 
 @app.route('/')
 def default():
 	return render_template('Web.html')
